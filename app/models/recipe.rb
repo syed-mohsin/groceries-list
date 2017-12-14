4c8f0d84bd14b9eb5d0f50aa24273c886d900328
Syed Mohsin <syedm.90@gmail.com>
2017-12-13 19:49:59 -0500

    make sure all associated models are deleted when deleting recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ActiveRecord::Base
   has_many :recipe_steps
   has_many :recipe_ingredients
-  has_many :steps, through: :recipe_steps
-  has_many :ingredients, through: :recipe_ingredients
+  has_many :steps, through: :recipe_steps, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients, dependent: :destroy
 
   default_scope { order(created_at: :asc) }
 end
